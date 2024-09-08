5c506ff7cf818f95bae0639a0420ffeca29c775a
brianmbe <briannmbe@gmail.com>
2024-09-08 18:00:06 +0300

    mutate cabins

---
+++ src/pages/Cabin.jsx
@@ -0,0 +1,18 @@
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import CabinTable from "../features/cabins/CabinTable";
+
+export default function Cabins() {
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as={"h1"}>All Cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
+  );
+}
