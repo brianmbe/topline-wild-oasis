f4b55a737d7f0c1e27ee85b182889b7ecfc47a2b
brianmbe <briannmbe@gmail.com>
2024-10-05 14:50:51 +0300

    implement today's activity

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -8,6 +8,7 @@ import Stats from "./stats/Stats";
 import useCabins from "../cabins/hooks/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -38,6 +39,7 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins?.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
