e2918ce61b483aabee2d748582f448f2a109615e
brianmbe <briannmbe@gmail.com>
2024-11-10 13:32:01 +0300

    handling dynamic metadata & not-found handling

--- the-wild-oasis-nextjs/app/cabins/[cabinId]/page.jsx
+++ the-wild-oasis-nextjs/app/cabins/[cabinId]/page.jsx
@@ -2,34 +2,44 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+// Dynamic Metadata
+export async function generateMetadata({ params }) {
+  const { name: cabinName } = await getCabin(params.cabinId);
+  return {
+    title: `Cabin ${cabinName}`,
+  };
+}
+
 export default async function Page({ params }) {
-  const cabin = await getCabin(params.cabinId);
+  const { image, name, description, maxCapacity } = await getCabin(
+    params.cabinId
+  );
   console.log(params);
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-        <div className="relative scale-[1.15] -translate-x-3">
+        <div className="relative scale-[1.15] -translate-x-3 w-full h-full">
           <Image
             fill
             className="object-cover"
-            src={cabin.image}
-            alt={`Cabin ${cabin.name}`}
+            src={image}
+            alt={`Cabin ${name}`}
           />
         </div>
 
         <div>
           <h3 className="text-accent-600 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
-            Cabin {cabin.name}
+            Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{cabin.description}</p>
+          <p className="text-lg text-primary-300 mb-10">{description}</p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
               <UsersIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                For up to <span className="font-bold">{cabin.maxCapacity}</span>{" "}
+                For up to <span className="font-bold">{maxCapacity}</span>{" "}
                 guests
               </span>
             </li>
